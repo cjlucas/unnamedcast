9e65abdd8cc1ff6b78bbf9e7b1f3e6c104ef7a6c
Chris Lucas <chris@chrisjlucas.com>
2016-04-23 20:27:43 -0700

    Initial commit of api cli tool

---
+++ src/github.com/cjlucas/unnamedcast/tools/api/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/cjlucas/unnamedcast/koda"
+	"github.com/cjlucas/unnamedcast/worker/api"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: api <url>")
+		os.Exit(1)
+	}
+
+	koda.Configure(&koda.Options{
+		URL: "redis://192.168.1.21:6379",
+	})
+
+	baseURL, err := url.Parse("http://192.168.1.21")
+	if err != nil {
+		panic(err)
+	}
+	apiTransport := api.API{BaseURL: baseURL}
+
+	url := os.Args[1]
+	feed, err := apiTransport.CreateFeed(&api.Feed{
+		URL: url,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(feed)
+	koda.Submit("update-feed", 100, map[string]string{
+		"feed_id": feed.ID,
+	})
+}
