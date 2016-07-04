f6f3735e85842f9a010520b349da721d2fd1becb
Chris Lucas <chris@chrisjlucas.com>
2016-07-04 15:57:16 -0700

    Removed koda dependency on primer

--- src/github.com/cjlucas/unnamedcast/tools/primer/main.go
+++ src/github.com/cjlucas/unnamedcast/tools/primer/main.go
@@ -4,18 +4,15 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/cjlucas/koda-go"
 	"github.com/cjlucas/unnamedcast/api"
 )
 
 var apiHost = flag.String("api-host", "localhost:80", "Host for API")
-var rdbURL = flag.String("redis-url", "redis://localhost:6379", "URL for redis endpoint")
 
 func main() {
 	flag.Parse()
 
 	apiTransport := api.API{Host: *apiHost}
-	koda.Configure(&koda.Options{URL: *rdbURL})
 
 	fmt.Println("Creating user")
 	user, err := apiTransport.CreateUser("chris", "blah")
@@ -45,8 +42,10 @@ func main() {
 		}
 		user.FeedIDs = append(user.FeedIDs, feed.ID)
 
-		koda.Submit("update-feed", 100, map[string]string{
-			"feed_id": feed.ID,
+		apiTransport.CreateJob(&api.Job{
+			Queue:    "update-feed",
+			Priority: 100,
+			Payload:  map[string]string{"feed_id": feed.ID},
 		})
 	}
 
