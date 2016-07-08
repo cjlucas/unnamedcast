343e507681fe3d0025ed11a4b6dd79b21c8ff186
Chris Lucas <chris@chrisjlucas.com>
2016-07-08 11:38:50 -0700

    Added initial jobs code

--- dashboard/js/jobs.jsx
+++ dashboard/js/jobs.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
 
-export class JobList extends React.Component {
+class JobEntry extends React.Component {
+  doit() {
+    console.log('got clicked', this.props.id);
+  }
+
   render() {
     return (
-      <h1>Hi world</h1>
+      <tr onClick={this.doit.bind(this)}>
+        <td>{this.props.id}</td>
+        <td>{this.props.queue}</td>
+        <td>{this.props.state}</td>
+        <td>{this.props.completionTime}</td>
+      </tr>
+    );
+  }
+}
+
+export class JobList extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      jobs: []
+    };
+  }
+
+  componentWillMount() {
+    this.fetchJobs();
+  }
+
+  fetchJobs() {
+    fetch('/api/jobs?limit=20')
+    .then(resp => resp.json())
+    .then(data => this.setState({jobs: data}));
+  }
+
+  render() {
+    console.log(this.state);
+    var jobs = this.state.jobs.map(job => {
+      return (
+        <JobEntry
+          key={job.id}
+          id={job.id}
+          queue={job.queue}
+          state={job.state}
+          completionTime={job.completion_time}/>
+      );
+    });
+
+    return (
+      <table className="ui celled table">
+        <thead>
+          <tr>
+            <th>Job ID</th>
+            <th>Queue</th>
+            <th>Status</th>
+            <th>Completion Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobs}
+        </tbody>
+      </table>
     );
   }
 }
