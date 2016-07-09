68fa479c8d8137b527268baa76eda8562695a7cb
Chris Lucas <chris@chrisjlucas.com>
2016-07-08 23:10:52 -0700

    Added state filters

--- dashboard/js/jobs.jsx
+++ dashboard/js/jobs.jsx
@@ -1,23 +1,115 @@
 import React from 'react';
-import $ from 'jquery';
-
-console.log($);
+import _ from 'lodash';
+import classNames from 'classnames';
 
 class JobEntry extends React.Component {
-  doit() {
-    console.log('got clicked', this.props.id);
-    $('.ui.modal').modal('show');
+  render() {
+    var title;
+    var icon;
+    switch(this.props.state) {
+    case "finished":
+      title = "Finished";
+      icon = "checkmark";
+      break;
+    case "queued":
+      title = "Queued";
+      icon = "hourglass half";
+      break;
+    case "dead":
+      title = "Dead";
+      icon = "remove";
+      break;
+    case "working":
+      title = "Working";
+      icon = "refresh";
+    default:
+      title = `Unknown: ${this.props.state}`;
+      icon = "help";
+    }
+
+    icon += " icon";
+
+    return (
+      <tr>
+        <td style={{textAlign: 'center'}} className="collapsing">
+          <i title={title} className={icon}></i>
+        </td>
+        <td className="collapsing">{this.props.id}</td>
+        <td className="collapsing">{this.props.queue}</td>
+        <td className="mono">{JSON.stringify(this.props.payload)}</td>
+        <td className="collapsing">{this.props.completionTime}</td>
+      </tr>
+    );
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    var cls = {
+      ui: true,
+      button: true,
+      basic: !this.props.selected,
+    }
+    cls[this.props.color] = true;
+
+    cls = classNames(cls);
+
+    return (
+      <button className={cls} onClick={() => this.props.onClick(this.props.name)}>
+        {this.props.text}
+      </button>
+    );
+  }
+}
+
+class QueueFilterButtons extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      buttonStates: {
+        queued: false,
+        working: false,
+        finished: false,
+        dead: false,
+      }
+    };
+  }
+
+  onButtonClick(key) {
+    var states = {
+      queued: false,
+      working: false,
+      finished: false,
+      dead: false,
+    };
+    states[key] = true;
+    this.setState({buttonStates: states});
+    this.props.onChange(key);
   }
 
   render() {
-    console.log(this.refs);
+    var buttons = [
+      {key: "queued", text: "Queued", color: "brown"},
+      {key: "working", text: "Working", color: "purple"},
+      {key: "finished", text: "Finished", color: "green"},
+      {key: "dead", text: "Dead", color: "red"},
+    ].map(info => {
+      return (
+        <Button
+          onClick={this.onButtonClick.bind(this)}
+          name={info.key}
+          key={info.key}
+          selected={this.state.buttonStates[info.key]}
+          text={info.text}
+          color={info.color} />
+        );
+      });
+
     return (
-      <tr onClick={this.doit.bind(this)}>
-        <td>{this.props.id}</td>
-        <td>{this.props.queue}</td>
-        <td>{this.props.state}</td>
-        <td>{this.props.completionTime}</td>
-      </tr>
+      <div>
+        {buttons}
+      </div>
     );
   }
 }
@@ -26,22 +118,42 @@ export class JobList extends React.Component {
   constructor() {
     super(...arguments);
     this.state = {
-      jobs: []
+      jobs: [],
+      stateFilters: [],
     };
   }
 
   componentWillMount() {
     this.fetchJobs();
+    setInterval(this.fetchJobs.bind(this), 5000)
+  }
+
+  onStateFilterChanged(selectedStates) {
+    this.state.stateFilters = [selectedStates];
+    this.fetchJobs();
+    this.setState(this.state);
   }
 
   fetchJobs() {
-    fetch('/api/jobs?limit=20')
+    var params = {
+      limit: 20,
+    }
+
+    if (this.state.stateFilters.length > 0) {
+      params.state = this.state.stateFilters[0];
+    }
+
+    var s = '';
+    Object.keys(params).forEach(key => s += `${key}=${params[key]}&`)
+
+    console.log(s);
+
+    fetch(`/api/jobs?${s}`)
     .then(resp => resp.json())
-    .then(data => this.setState({jobs: data}));
+    .then(data => this.setState({jobs: data || []}));
   }
 
   render() {
-    console.log(this.state);
     var jobs = this.state.jobs.map(job => {
       return (
         <JobEntry
@@ -49,24 +161,30 @@ export class JobList extends React.Component {
           id={job.id}
           queue={job.queue}
           state={job.state}
+          payload={job.payload}
           completionTime={job.completion_time}/>
       );
     });
 
     return (
-      <table className="ui celled table" ref={c => console.log(c)}>
-        <thead>
-          <tr>
-            <th>Job ID</th>
-            <th>Queue</th>
-            <th>Status</th>
-            <th>Completion Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jobs}
-        </tbody>
-      </table>
+      <div>
+        <QueueFilterButtons onChange={this.onStateFilterChanged.bind(this)}/>
+
+        <table className="ui celled table">
+          <thead>
+            <tr>
+              <th>State</th>
+              <th>Job ID</th>
+              <th>Queue</th>
+              <th>Payload</th>
+              <th>Completion Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
