586c29a3fb0870336d19860f72fb85d081770135
Chris Lucas <chris@chrisjlucas.com>
2016-07-23 13:31:25 -0700

    Removed embedded time.Time

--- src/github.com/cjlucas/unnamedcast/db/utctime/utctime.go
+++ src/github.com/cjlucas/unnamedcast/db/utctime/utctime.go
@@ -8,33 +8,41 @@ import (
 )
 
 type Time struct {
-	time.Time
+	t time.Time
 }
 
 func (t *Time) UnmarshalJSON(text []byte) error {
-	err := json.Unmarshal(text, &t.Time)
+	err := json.Unmarshal(text, &t.t)
 	if err != nil {
 		return err
 	}
 
-	t.Time = t.UTC()
+	t.t = t.t.UTC()
 	return nil
 }
 
-func (t Time) Add(dur time.Duration) Time {
-	return Time{t.Time.Add(dur)}
+func (t *Time) IsZero() bool {
+	return t.t.IsZero()
+}
+
+func (t *Time) Format(layout string) string {
+	return t.t.UTC().Format(layout)
+}
+
+func (t *Time) Add(dur time.Duration) Time {
+	return Time{t.t.Add(dur)}
 }
 
 func (t *Time) Before(time Time) bool {
-	return t.Time.UTC().Before(time.Time.UTC())
+	return t.t.UTC().Before(time.t.UTC())
 }
 
 func (t Time) Equal(time Time) bool {
-	return t.UTC() == time.UTC()
+	return t.t.UTC() == time.t.UTC()
 }
 
 func (t *Time) MarshalJSON() ([]byte, error) {
-	return json.Marshal(t.UTC())
+	return json.Marshal(t.t.UTC())
 }
 
 // When mgo.v2/bson checks if a concrete type is a Getter, it does not check if its
@@ -44,16 +52,16 @@ func (t *Time) MarshalJSON() ([]byte, error) {
 // UTC for us
 
 func (t Time) GetBSON() (interface{}, error) {
-	return t.UTC(), nil
+	return t.t.UTC(), nil
 }
 
 func (t *Time) SetBSON(raw bson.Raw) error {
-	err := raw.Unmarshal(&t.Time)
+	err := raw.Unmarshal(&t.t)
 	if err != nil {
 		return err
 	}
 
-	t.Time = t.UTC()
+	t.t = t.t.UTC()
 	return nil
 }
 
